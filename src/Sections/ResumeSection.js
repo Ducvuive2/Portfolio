c16e7364b305fa4df9b057c07d0e9681697b79c4
duc.nguyenvu <duc20062001@gmail.com>
2025-03-04 17:27:03 +0700

    Update Resume Section

--- src/Sections/ResumeSection.js
+++ src/Sections/ResumeSection.js
@@ -7,7 +7,7 @@ function ResumeSection() {
   const handleDownloadCV = () => {
     toast.info("You don't have permission to download CV. Please contact via email for more information.", {
       position: "top-center",
-      autoClose: 5000,
+      autoClose: 3000,
       hideProgressBar: false,
       closeOnClick: true,
       pauseOnHover: true,
