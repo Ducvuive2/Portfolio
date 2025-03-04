d6e2c86c73f5ae89092d092bf585e8305fdf0f32
duc.nguyenvu <duc20062001@gmail.com>
2025-03-04 16:44:39 +0700

    Obfuscated code

--- obfuscate.js
+++ obfuscate.js
@@ -2,29 +2,27 @@ const JavaScriptObfuscator = require('javascript-obfuscator');
 const fs = require('fs');
 const path = require('path');
 
-const isProduction = process.env.NODE_ENV === 'production';
-
-// Paths
+// Path to the source file
 const portfolioPath = path.join(__dirname, 'src', 'Portfolio.js');
-const portfolioOutputPath = portfolioPath; // We'll overwrite the same file in production
-const devOutputPath = path.join(__dirname, 'src', 'Portfolio.obfuscated.js');
 
-try {
-  const code = fs.readFileSync(portfolioPath, 'utf8');
-
-  if (isProduction) {
-    // Obfuscate & overwrite the original file
+if (process.env.NODE_ENV === 'production') {
+  try {
+    const code = fs.readFileSync(portfolioPath, 'utf8');
     const obfuscatedCode = JavaScriptObfuscator.obfuscate(code, {
       compact: true,
       controlFlowFlattening: true
     }).getObfuscatedCode();
 
-    fs.writeFileSync(portfolioOutputPath, obfuscatedCode);
-    console.log('Production build: Portfolio.js has been obfuscated and overwritten.');
-  } else {
-    console.log('Development build: No obfuscation performed.');
+    // Prepend ESLint disable comment
+    const output = '/* eslint-disable */\n' + obfuscatedCode;
+
+    // Overwrite Portfolio.js with the obfuscated version
+    fs.writeFileSync(portfolioPath, output);
+    console.log('Portfolio.js has been obfuscated in place.');
+  } catch (error) {
+    console.error('Error during obfuscation:', error);
+    process.exit(1);
   }
-} catch (error) {
-  console.error('Error during obfuscation:', error);
-  process.exit(1);
-} 
+} else {
+  console.log('Skipping obfuscation in development.');
+}
